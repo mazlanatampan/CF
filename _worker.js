1f0690cc08117f7928c675ef37305f01a2277eb9
mazlanatampan <datamazlana@gmail.com>
2025-01-02 01:11:57 +0700

    Update

--- _worker.js
+++ _worker.js
@@ -1816,12 +1816,12 @@ class Document {
     let flagElement = '<div class="card-container">';
 for (const flag of new Set(flagList)) {
   flagElement += `
-    <div class="card">
+    <div class="card" onclick="window.location.href='/sub?cc=${flag}${proxyBankUrl ? "&proxy-list=" + proxyBankUrl : ""}'">
       <a href="/sub?cc=${flag}${proxyBankUrl ? "&proxy-list=" + proxyBankUrl : ""}" class="country-flag">
         <img width="32" src="https://hatscripts.github.io/circle-flags/flags/${flag.toLowerCase()}.svg" alt="${flag} Flag"/>
       </a>
       <div class="info-text">
-      <i class="bx bx-globe> COUNTRY : ${flag}</i>
+      <i class="bx bx-globe"> COUNTRY : ${flag}</i>
       <i class='bx bxs-microchip'> TOTAL IP : ${countryCount[flag]} IP
       </i>
       </div> 
