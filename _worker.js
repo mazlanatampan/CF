36d738f7df50729eaa05db4e14ae45cb1a01ca0d
mazlanatampan <datamazlana@gmail.com>
2025-01-02 20:22:42 +0700

    Update

--- _worker.js
+++ _worker.js
@@ -1554,10 +1554,12 @@ let baseHTML = `
 
 
 #content {
-  flex-grow: 1; /* Membuat elemen ini mengisi ruang yang tersisa */
-  overflow: auto; /* Menambahkan scroll jika konten melebihi ruang */
-  background-color: rgba(255, 255, 255, 0.1); /* Opsional: Warna latar */
-  padding: 20px; /* Opsional: Padding untuk konten */
+  width: 100%; /* Lebar penuh */
+  height: calc(100vh - 60px); /* Kurangi tinggi header jika ada */
+  margin: 0; /* Hilangkan margin */
+  padding: 0; /* Hilangkan padding */
+  overflow: auto; /* Untuk menggulir jika konten lebih besar dari viewport */
+  box-sizing: border-box; /* Sertakan padding dan border dalam ukuran total */
 }
 
 
@@ -1804,6 +1806,8 @@ function loadContent(path) {
     })
     .then((html) => {
       document.getElementById("content").innerHTML = html;
+      content.style.height = `calc(100vh - ${document.getElementById("header-atas").offsetHeight}px)`;
+  
     })
     .catch((error) => {
       console.error("Error fetching content:", error);
