e3347e939d17810b693830e145e140a6917abf03
mazlanatampan <datamazlana@gmail.com>
2025-01-03 21:45:52 +0700

    Update

--- _worker.js
+++ _worker.js
@@ -1953,7 +1953,7 @@ class Document {
           <i class='bx bxs-microchip'> TOTAL IP : ${count} IP</i>
         </div>`;
     }
-        let flagElement = '</div>';
+        flagElement = '</div>';
     this.html = this.html.replaceAll("PLACEHOLDER_BENDERA_NEGARA", flagElement);
 }
 
